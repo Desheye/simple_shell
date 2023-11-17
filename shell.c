5028a9f5bc9e7d34c398b40c938f3b1a0eb66402
Desheye <aarealaafiaboyz@gmail.com>
2023-11-17 16:48:35 +0100

    deadline entry again

---
+++ shell.c
@@ -0,0 +1,65 @@
+#include "shell.h"
+
+#define MAX_INPUT_SIZE 1024
+
+void execute_command(char *command) {
+    /* Tokenize the command */
+    char *args[MAX_INPUT_SIZE];
+    char *token = strtok(command, " \t\n");
+    int i = 0;
+
+    while (token != NULL) {
+        args[i++] = token;
+        token = strtok(NULL, " \t\n");
+    }
+
+    args[i] = NULL;
+
+    /* Declare pid_t at the beginning of the block */
+    pid_t pid;
+
+    /* Fork a child process */
+    pid = fork();
+
+    if (pid == 0) {
+        /* Child process */
+        if (execvp(args[0], args) == -1) {
+            perror("Error executing command");
+        }
+        exit(EXIT_FAILURE);
+    } else if (pid > 0) {
+        /* Parent process */
+        wait(NULL); /* Wait for the child process to complete */
+    } else {
+        perror("Error forking process");
+    }
+}
+
+int main() {
+    char input[MAX_INPUT_SIZE];
+
+    while (1) {
+        /* Display the prompt */
+        printf("$ ");
+        fflush(stdout);
+
+        /* Read user input */
+        if (fgets(input, sizeof(input), stdin) == NULL) {
+            break; /* Exit on EOF (e.g., Ctrl+D) */
+        }
+
+        /* Remove the newline character */
+        input[strcspn(input, "\n")] = '\0';
+
+        /* Exit the shell if the user enters "exit" or "quit" */
+        if (strcmp(input, "exit") == 0 || strcmp(input, "quit") == 0) {
+            break;
+        }
+
+        /* Execute the command */
+        execute_command(input);
+    }
+
+    return 0;
+}
+
